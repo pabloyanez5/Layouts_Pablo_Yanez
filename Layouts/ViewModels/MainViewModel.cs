2739b9be4520b9c5d9e739de7d1c201665ca4322
pabloyanez5 <130101518+pabloyanez5@users.noreply.github.com>
2024-11-20 00:13:46 -0500

    Commit 4

--- Layouts/ViewModels/MainViewModel.cs
+++ Layouts/ViewModels/MainViewModel.cs
@@ -5,13 +5,11 @@ namespace Layouts.ViewModels
 {
     internal class MainViewModel
     {
-        // Comandos para navegar entre las páginas
         public ICommand GoToGridPageCommand { get; }
         public ICommand GoToStackPageCommand { get; }
         public ICommand GoToFlexPageCommand { get; }
         public ICommand GoToAbsolutePageCommand { get; }
 
-        // Constructor donde se inicializan los comandos
         public MainViewModel()
         {
             GoToGridPageCommand = new Command(async () => await Shell.Current.GoToAsync("///GridPage"));
